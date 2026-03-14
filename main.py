5ae41642dace23085fe6de68730302bb1a30367b
santosh-sunuwar-mukhiya <santoshsunuwarmukhiya@gmail.com>
2026-03-14 16:26:48 +0545

    Added the feature to delete the post using post id

--- main.py
+++ main.py
@@ -220,8 +220,20 @@ def update_post_partial(post_id: int, post_data: PostUpdate, db: Annotated[Sessi
     db.commit()
 
     return post
-    
 
+@app.delete("/api/posts/{post_id}", status_code = status.HTTP_204_NO_CONTENT)
+def delete_post(post_id: int, db: Annotated[ Session, Depends(get_db)]):
+    result = db.execute(select(models.Post).where(models.Post.id == post_id))
+    post = result.scalars().first()
+
+    if not post:
+        raise HTTPException(
+            status_code = status.HTTP_404_NOT_FOUND, 
+            detail = "Post not found."
+        )
+    
+    db.delete(post)
+    db.commit()
 
 
 @app.exception_handler(StarletteHTTPException)
